f4954c7f041e3bba921a21b531b3c015f0d4ab10
Marko Grešak <marko@gresak.io>
2015-05-22 19:49:18 +0200

    fix a typo in test
    Lesson learned: run grunt before pushing.

--- test/test-canihazip.js
+++ test/test-canihazip.js
@@ -10,7 +10,7 @@ describe('canihazip node module', function () {
   it('should return a string matching IPv4', function () {
     var IP4Regexp = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
     var ip = canihazip();
-    ip.shoud.be.a('string');
+    ip.should.be.a('string');
     ip.should.match(IP4Regexp);
   });
 
