c0a09645a861430f140a7dcf108e8b6d36b806df
Marko Grešak <marko@gresak.io>
2015-05-22 21:57:54 +0200

    add grunt bump task

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,22 @@ module.exports = function (grunt) {
         files: '<%= jshint.test.src %>',
         tasks: ['jshint:test', 'mochacli']
       }
-    }
+    },
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+      }
+    },
   });
 
   grunt.registerTask('default', ['jshint', 'mochacli']);
