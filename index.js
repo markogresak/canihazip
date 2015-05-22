24a9a8490ec7d61785ad0eac06f8b1876589a84c
Marko Grešak <marko@gresak.io>
2015-05-22 21:41:51 +0200

    implement server request, return data to callback

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
 'use strict';
-module.exports = function () {
-  return '8.8.8.8';
-};
+
+/**
+ * Import HTTP interface module.
+ *
+ * TODO: Implement HTTPS. - has to be implemented on canihazip.com first.
+ */
+var protocol = require('http');
+
+/**
+ * CanIHazIp constructor function.
+ *
+ * @param {Function=} callback (optional) Function to be called with IP data after server responds.
+ */
+function CanIHazIp(callback) {
+
+  var httpCallback = function (response) {
+    var responseData = '';
+    // When data is received, append it to `responseData`.
+    response.on('data', function (chunk) {
+      responseData += chunk;
+    });
+    // When response ends, call `callback` if it's a function.
+    response.on('end', function () {
+      if (typeof callback === 'function') {
+        callback(responseData);
+      }
+    });
+  };
+
+  // Connect options for canihazip API, GET method is implicit.
+  var canihazipOptions = {
+    host: 'canihazip.com',
+    path: '/s'
+  };
+
+  // Make a request to canihazip and end it immediately.
+  protocol.request(canihazipOptions, httpCallback).end();
+}
+
+module.exports = CanIHazIp;
--- test/test-canihazip.js
+++ test/test-canihazip.js
@@ -1,17 +1,15 @@
 /* jshint expr: true */
 'use strict';
 var chai = require('chai');
+var sinon = require('sinon');
 chai.should();
+chai.use(require('sinon-chai'));
 
 var canihazip = require('../');
 
 describe('canihazip node module', function () {
 
-  it('should return a string matching IPv4', function () {
     var IP4Regexp = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    var ip = canihazip();
-    ip.should.be.a('string');
-    ip.should.match(IP4Regexp);
   });
 
 });
